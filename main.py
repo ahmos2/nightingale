c3731693ed21478fa9df566129737c74b7a614df
Martin Sundhaug <martinsundhaug@gmail.com>
2015-03-19 06:13:42 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import cherrypy,os
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket,EchoWebSocket
+
+cherrypy.config.update({'server.socket_host' : '0.0.0.0'})
+WebSocketPlugin(cherrypy.engine).subscribe()
+cherrypy.tools.websocket = WebSocketTool()
+
+class WSHandler(WebSocket):
+    def received_message(self, message):
+        self.send(message.data, message.is_binary)
+
+class watchdog(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+    @cherrypy.expose
+    def Alive(self,company,ship,controller,instance,day,ms):
+        print company,ship,controller,instance,day,ms
+        cherrypy.engine.publish('websocket-broadcast',ms)
+        return "ok"
+    @cherrypy.expose
+    def Alert(self,company,ship,controller,instance,error):
+        print company,ship,controller,instance,error
+        return "ok"
+    @cherrypy.expose
+    def ws(self):
+        cherrypy.request.ws_handler
+
+
+cherrypy.config.update(
+                       {
+                           '/static' : 
+                               {
+                                   'tools.staticdir.root': os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+                               }
+                       }
+)
+                       
+
+
+print cherrypy.tools.websocket
+cherrypy.quickstart(watchdog(),"/",config={
+        '/ws': {
+            'tools.websocket.on': True,
+            'tools.websocket.handler_cls': WSHandler
+            }
+        })
