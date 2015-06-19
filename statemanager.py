5f0faaa8eb4830a6fcb6c9ec11631d1658de38e8
Martin Sundhaug <martinsundhaug@gmail.com>
2015-06-20 01:54:44 +0200

    Switched StateManager to redis

--- statemanager.py
+++ statemanager.py
@@ -1,14 +1,10 @@
+import redis
 class StateManager(object):
-    instanceState={}
+    def __init__(self):
+        self.redis=redis.Redis()
     def get(self,instanceName, key):
-        if not self.instanceState.has_key(instanceName):
-            return None
-        if not self.instanceState[instanceName].has_key(key):
-            return None
-        return self.instanceState[instanceName][key]
+        return self.redis.get(instanceName+"."+key)
     def set(self,instanceName, key, value):
-        if not self.instanceState.has_key(instanceName):
-            self.instanceState[instanceName]={}
-        self.instanceState[instanceName][key]=value
+        self.redis.set(instanceName+"."+key, value)
     def UniqueName(self,company,ship,controller,instance):
         return company+":"+ship+":"+controller+":"+instance
