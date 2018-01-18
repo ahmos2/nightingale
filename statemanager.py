4787ce7cd5875462d89f854bd8591b57814ca21c
Martin Sundhaug <MartinSundhaug@gmail.com>
2018-01-18 03:35:40 +0100

    FIX: Missing self ref

--- statemanager.py
+++ statemanager.py
@@ -1,10 +1,10 @@
 import redis
 class StateManager(object):
     def __init__(self):
-        rcon=redis.Redis()
+        self.rcon=redis.Redis()
     def get(self,instanceName, key):
-        return rcon.get(instanceName+"."+key)
+        return self.rcon.get(instanceName+"."+key)
     def set(self,instanceName, key, value):
-        rcon.set(instanceName+"."+key,value)
+        self.rcon.set(instanceName+"."+key,value)
     def UniqueName(self,company,ship,controller,instance):
         return company+":"+ship+":"+controller+":"+instance
